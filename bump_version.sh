7a29a6b5b1cf368af20388b3552896f3644f2646
Vitaliy Kudryk <kudryk.v@protonmail.com>
2021-05-27 10:36:50 +0300

    work on bump_version.sh

---
+++ bump_version.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -e
+
+_version=$(grep -A 1 version info.plist | tail -n 1 | perl -pe 's/.*(\d+\.\d+\.\d+).*/\1/')
+if [ -z "${_version}" ]; then
+  echo 'Could not parse version from info.plist'
+  exit 1
+fi
+
+_line_number=$(grep -n "${_version}" info.plist | cut -d: -f1)
+if [ -z "${_line_number}" ]; then
+  echo 'Could not figure out line number with version'
+  exit 1
+fi
+
+_major=$(echo "${_version}" | cut -d. -f1)
+_minor=$(echo "${_version}" | cut -d. -f2)
+_patch=$(echo "${_version}" | cut -d. -f3)
+
+case "$1" in
+patch)
+  _patch=$((_patch + 1))
+  ;;
+
+minor)
+  _minor=$((_minor + 1))
+  _patch=0
+  ;;
+
+major)
+  _major=$((_major+1))
+  _minor=0
+  _patch=0
+  ;;
+
+*)
+  echo "unknown mod $1"
+  exit 1
+  ;;
+esac
+
+_bump=${_major}.${_minor}.${_patch}
+
+sed -i'.old' -e "${_line_number}s/${_version}/${_bump}/" info.plist
+
+git commit -m "update version from ${_version} to ${_bump}"
+git tag -a "v${_bump}"
